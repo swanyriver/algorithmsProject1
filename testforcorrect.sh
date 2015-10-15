854aba1450048a93a875c37f60d8adead4d30907
Brandon Swanson <swanyriver@gmail.com>
2015-10-15 13:30:10 -0700

    all results match

---
+++ testforcorrect.sh
@@ -0,0 +1,10 @@
+echo "Difference between sample output and algorithm output"
+echo "ALGORITHM 1"
+diff algo1.txt input_output/output.txt
+echo "ALGORITHM 2"
+diff algo2.txt input_output/output.txt
+echo "ALGORITHM 3"
+diff algo3.txt input_output/output.txt
+echo "ALGORITHM 4"
+diff algo4.txt input_output/output.txt
+
